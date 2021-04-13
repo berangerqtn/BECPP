807dadfd41c552934ab82bfa0a260478ea02b0ab
Béranger <beranger.quintana@gmail.com>
2021-04-13 09:26:13 +0200

    Ajout de la librairie RGB_LCD

---
+++ include/user.h
@@ -0,0 +1,14 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+class user {
+
+private:
+
+}
+
+
+
+#endif //USER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 //#include "MyWebServer.h"
 #include "Arduino.h"
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
 
 void setup() {
   // put your setup code here, to run once:
   //setup_ESP();
   //setup_WebServer();
   //digitalWrite(LED_BUILTIN,HIGH);
+
+  //LCD
+  Serial.begin(9600);
+  lcd.begin(16,2);
 }
 
 void loop() {
+
+  if (Serial.available()){
+    lcd.clear();
+    lcd.print("Hello Beauté");
+  }
   // put your main code here, to run repeatedly:
   //if (WiFi.isConnected())
     //WebServer.handleClient();
